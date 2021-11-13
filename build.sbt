a1982b60fe40286d8130ef5f10ee266ede530305
Aleksandr Lazarchuk <aleksandr.lazarchuk@escalibre.net>
2021-11-13 06:30:16 +0200

    #dev

--- build.sbt
+++ build.sbt
@@ -2,17 +2,28 @@ name         := "slick-example"
 version      := "0.1.0-SNAPSHOT"
 scalaVersion := "2.12.8"
 
+//scalaVersion := "2.12.6"
+//scalaVersion := "2.13.6"
+
 val akkaVer           = "2.5.20"
+//val akkaVer           = "2.6.17"
 val akkaHttpVer       = "10.1.7"
+//val akkaHttpVer       = "10.2.7"
 val slickVer          = "3.3.0"
+//val slickVer          = "3.3.3"
 val postgresVer       = "42.2.5"
 val json4sVer         = "3.6.4"
+//val json4sVer         = "4.0.3"
 val akkaHttpJson4sVer = "1.25.2"
+//val akkaHttpJson4sVer = "1.38.2"
 val macwireVer        = "2.4.1"
 val pureConfigVer     = "0.9.0"
-//val akkaQuartzVer     = "1.6.1-akka-2.5.x"
+//val pureConfigVer     = "0.17.0"
+val akkaQuartzVer     = "1.7.1-akka-2.5.x"
+//val akkaQuartzVer     = "1.9.2-akka-2.6.x"
 val logbackClassicVer = "1.2.3"
 val scalaTestVer      = "3.0.8-RC4"
+//val scalaTestVer      = "3.3.0-SNAP3"
 
 libraryDependencies ++= Seq(
   "com.typesafe.akka"        %% "akka-actor"            % akkaVer,
@@ -27,6 +38,6 @@ libraryDependencies ++= Seq(
 //  "com.enragedginger"        %% "akka-quartz-scheduler" % akkaQuartzVer,
   "com.github.pureconfig"    %% "pureconfig"            % pureConfigVer,
   "ch.qos.logback"           %  "logback-classic"       % logbackClassicVer,
-  "org.scalatest"            %% "scalatest"             % scalaTestVer % Test
+  "org.scalatest"            %% "scalatest"             % scalaTestVer       % Test
 )
 
--- src/main/scala/kz/example/BookApp.scala
+++ src/main/scala/kz/example/BookApp.scala
@@ -1,17 +1,23 @@
 package kz.example
 
-import akka.actor.ActorSystem
+import akka.actor.{Actor, ActorSystem, Props}
+import akka.http.scaladsl.settings.ServerSettings
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Directives.pathPrefix
 import akka.http.scaladsl.server.Route
 import akka.stream.ActorMaterializer
-import com.typesafe.config.{Config, ConfigFactory}
 import kz.example.repository.BookRepositoryImpl
 import kz.example.routers.BookRoute
 import org.slf4j.LoggerFactory
+
 import scala.concurrent.ExecutionContext
 import com.softwaremill.macwire.wire
 import kz.example.config.{actorSystemName, appConfig}
+import kz.example.config.actorSystemName
+
+import java.time.Duration
+import java.util.concurrent.TimeUnit
+import scala.concurrent.duration.{Duration, FiniteDuration}
 
 
 object BookApp extends App {
@@ -23,6 +29,23 @@ object BookApp extends App {
   val log = LoggerFactory.getLogger(BookApp.getClass)
 
 
+
+
+  // https://stackoverflow.com/questions/9714831/how-can-i-have-an-akka-actor-executed-every-5-min
+  case class Message()
+  class MyActor extends Actor {
+    def receive = { case Message() => println("Do something in actor") }
+  }
+
+  val actor = system.actorOf(Props(new MyActor), name = "actor")
+  system.scheduler.schedule(
+    FiniteDuration(50, TimeUnit.MILLISECONDS),
+    FiniteDuration(1500, TimeUnit.MILLISECONDS),
+    actor,
+    Message())
+
+
+
   val bookRepository = wire[BookRepositoryImpl]
   val bookRoute = wire[BookRoute]
 
@@ -32,6 +55,8 @@ object BookApp extends App {
   }
 
   Http().bindAndHandle(routes, appConfig.application.host, appConfig.application.port)
+//  Http().newServerAt("0.0.0.0", 8082)
+
 
   log.info("{} ActorSystem started", actorSystemName)
 
